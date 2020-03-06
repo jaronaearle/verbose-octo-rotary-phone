31448caa429d28eed3a867f8e616a0e50a89dd79
Jaron Earle <jaron.earle@gmail.com>
2020-03-05 22:32:04 -0700

    nasty if else

--- assignmentFourArraysJaronEarle/src/assignmentfourarraysjaronearle/AssignmentFourArraysJaronEarle.java
+++ assignmentFourArraysJaronEarle/src/assignmentfourarraysjaronearle/AssignmentFourArraysJaronEarle.java
@@ -14,33 +14,60 @@ public class AssignmentFourArraysJaronEarle
         String sNum;
         String[] sNums;
         Scanner gpaData;
-        
+
         sNums = new String[1000];
         sGpa = new double[1000];
         gpaData = new Scanner(new File("studentdata.txt"));
-        
-        while (gpaData.hasNext()) 
+
+        // adds student data to arrays
+        for (int i = 0; gpaData.hasNext(); i++)
         {
-            int index = 0;
-           
             sNum = gpaData.next();
             gpa = gpaData.nextDouble();
-            
-            System.out.println("Fresh of the press " + sNum + "\t" + gpa);
-            
-            sNums[index] = sNum;
-            sGpa[index] = gpa;
-            
-            System.out.println("From the array " + sNums[index] + "\t" + sGpa[index]);
-            
-            index++;
+
+            sNums[i] = sNum;
+            sGpa[i] = gpa;
         }
         
-//        for (int i = 0; i < sNums.length; i++) 
-//        {
-//            System.out.println(sNums[i] + "\t" + sGpa[i]);
-//        }
-
+        for (int i = 0; i < sGpa.length; i++) 
+        {
+            if (sGpa[i] != 0) 
+            {
+                if (sGpa[i] < 0.5) 
+                {
+                    System.out.println("0.0 - 0.5");
+                }
+                else if (sGpa[i] >= 0.5 && sGpa[i] < 1.0) 
+                {
+                    System.out.println("0.5 - 1.0");
+                }
+                else if (sGpa[i] >= 1.0 && sGpa[i] < 1.5) 
+                {
+                    System.out.println("1.0 - 1.5");
+                }
+                else if (sGpa[i] >= 1.5 && sGpa[i] < 2.0) 
+                {
+                    System.out.println("1.5 - 2.0");
+                }
+                else if (sGpa[i] >= 2.0 && sGpa[i] < 2.5) 
+                {
+                    System.out.println("2.0 - 2.5");
+                }
+                else if (sGpa[i] >= 2.5 && sGpa[i] < 3.0) 
+                {
+                    System.out.println("2.5 - 3.0");
+                }
+                else if (sGpa[i] >= 3.0 && sGpa[i] < 3.5) 
+                {
+                    System.out.println("3.0 - 3.5");
+                }
+                else if (sGpa[i] >= 3.5 && sGpa[i] <= 4.0) 
+                {
+                    System.out.println("3.5 - 4.0");
+                }
+                       
+            }
+        }
     }
-    
 }
+
--- hangmanJaronEarle/src/hangmanjaronearle/HangmanJaronEarle.java
+++ hangmanJaronEarle/src/hangmanjaronearle/HangmanJaronEarle.java
@@ -1,20 +1,92 @@
 // Jaron Earle
 package hangmanjaronearle;
 
+import java.util.Scanner;
+
 public class HangmanJaronEarle
 {
 
     public static void main(String[] args)
     {
-         String[] words =
+        boolean gameOn = true;
+
+        String[] gameWords =
         {
             "javascript", "fuck", "fucking fuck", "goddamnit"
         };
-        
+
         char[] alphabet =
         {
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
         };
+
+        do
+        {
+            int guesses;
+            guesses = 0;
+
+            char guess;
+            char correctGuess;
+
+            boolean isGuessed = false; // for now 
+
+            String word = getWord(gameWords);
+            String playAgain;
+
+            char[] explodedWord;
+            explodedWord = explode(word);
+
+            Scanner input;
+            input = new Scanner(System.in);
+
+
+            while (!isGuessed)
+            {
+                displayGame(word, guesses);
+                
+                displayWord(word);
+                
+                System.out.println(word); // rm later
+                
+                System.out.println("Guess a letter: ");
+                guess = input.next().charAt(0);
+
+                for (char letter : explodedWord)
+                {
+                    if (letter == 'z') // update to isGuessed when loop works right
+                    {
+                        isGuessed = true;
+                        break;
+                        // update letters give winning message
+                    }
+                    else if (guess == letter) 
+                    {
+                        correctGuess = guess;
+                        break;
+                        // re-display word status && update available letters
+                        // call display game 
+                    }
+                    else if (guess != letter) 
+                    {
+                        
+                    }
+                }
+            }
+
+            if (isGuessed)
+            {
+                System.out.println("Play again? y/n:"); // EOG prompt for play again
+                playAgain = input.nextLine();
+
+                if (playAgain.equalsIgnoreCase("y"))
+                {
+                    gameOn = true;
+                }
+
+                gameOn = false;
+            }
+
+        } while (gameOn);
 
     }
 
@@ -67,23 +139,25 @@ public class HangmanJaronEarle
         System.out.println(hangman);
     }
 
-    public static boolean gameloop(String word, char[] alphabet)
+    public static String displayWord(String word)
     {
-        int numGuesses = 0; // 0 for now
-        boolean isGuessed = false;
-               
+        String newWord;
 
-        do {
-            displayGame(word,  numGuesses);
-            
-            for (int i = 0; i < word.length(); i++) 
-            {
-                
-            }
-            
-            
-        } while (!isGuessed);
+        return "";
+    }
 
-        return isGuessed;
+    public static char[] displayAlphabet(char[] alphabet, char letter)
+    {
+        char[] newAlphabet =
+        {
+        }; // initialize with values
+
+        return newAlphabet;
+    }
+
+    public static void clearConsole() 
+    {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
