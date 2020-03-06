ef1cdd647d91170d1f90936722889cce8ee92306
Jaron Earle <jaron.earle@gmail.com>
2020-03-05 22:55:16 -0700

    make count thing work

--- assignmentFourArraysJaronEarle/src/assignmentfourarraysjaronearle/AssignmentFourArraysJaronEarle.java
+++ assignmentFourArraysJaronEarle/src/assignmentfourarraysjaronearle/AssignmentFourArraysJaronEarle.java
@@ -29,44 +29,68 @@ public class AssignmentFourArraysJaronEarle
             sGpa[i] = gpa;
         }
         
-        for (int i = 0; i < sGpa.length; i++) 
+        displayGpaCount(sGpa);
+    }
+    
+    public static void displayGpaCount(double[] sGpa) 
+    {
+        int[] count;
+        count = new int[8];
+        
+        for (int j = 0; j < count.length; j++) 
+            {
+                count[j] = 0;
+            }
+            
+        for (int i = 0; i < sGpa.length; i++) // turn to method
         {
             if (sGpa[i] != 0) 
             {
                 if (sGpa[i] < 0.5) 
                 {
                     System.out.println("0.0 - 0.5");
+                    count[0] += 1;
                 }
                 else if (sGpa[i] >= 0.5 && sGpa[i] < 1.0) 
                 {
                     System.out.println("0.5 - 1.0");
+                    count[1] += 1;
                 }
                 else if (sGpa[i] >= 1.0 && sGpa[i] < 1.5) 
                 {
                     System.out.println("1.0 - 1.5");
+                    count[2] += 1;
                 }
                 else if (sGpa[i] >= 1.5 && sGpa[i] < 2.0) 
                 {
                     System.out.println("1.5 - 2.0");
+                    count[3] += 1;
                 }
                 else if (sGpa[i] >= 2.0 && sGpa[i] < 2.5) 
                 {
                     System.out.println("2.0 - 2.5");
+                    count[4] += 1;
                 }
                 else if (sGpa[i] >= 2.5 && sGpa[i] < 3.0) 
                 {
                     System.out.println("2.5 - 3.0");
+                    count[5] += 1;
                 }
                 else if (sGpa[i] >= 3.0 && sGpa[i] < 3.5) 
                 {
                     System.out.println("3.0 - 3.5");
+                    count[6] += 1;
                 }
                 else if (sGpa[i] >= 3.5 && sGpa[i] <= 4.0) 
                 {
                     System.out.println("3.5 - 4.0");
-                }
-                       
+                    count[7] += 1;
+                }      
             }
+        }
+        for (int i = 0; i < count.length; i++) 
+        {
+            System.out.println("Student count: " + count[i]);
         }
     }
 }
